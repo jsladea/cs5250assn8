2911f55d2ffa100017becdaf6ffd6672c72bdb37
jsladea <jaaronslade@gmail.com>
2024-12-11 19:15:36 -0700

    initial commit

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+import json
+import boto3
+import botocore
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
