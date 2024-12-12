249dea8c85b0230cb3424218e03fcbfbe01e8289
jsladea <jaaronslade@gmail.com>
2024-12-11 19:58:45 -0700

    Added error handling

--- lambda_function.py
+++ lambda_function.py
@@ -5,14 +5,27 @@ import logging
 
 from botocore.exceptions import ClientError
 
+
 queue_name = 'cs5250-requests'
 queue = None
 
 def lambda_handler(event, context):
-    # TODO implement
+    try:
+        if event.body:
+            return publish_widget(event.body)
+        else:
+            return get_error_response(400, 'Widget data not provided in request')
+    except ClientError:
+        return get_error_response(500, 'An error occurred sending widget to sqs queue')
+    except:
+        return get_error_response(500, 'An unexpected error was encountered')
+
+def get_error_response(status_code, msg):
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'statusCode': status_code,
+        'body': {
+            'error': msg
+        }
     }
 
 def publish_widget(widget_json_str):
@@ -22,6 +35,10 @@ def publish_widget(widget_json_str):
         )
     except ClientError as error:
         print(f"An error occurred sending {widget_json_str} to the sqs queue.")
+        raise error
     else:
         print(response)
-        return response
+        return {
+            'statusCode': 200,
+            'body': response
+        }
