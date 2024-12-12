69e3947dcb5edecb1e6e9bb0a3235e287af5a54e
jsladea <jaaronslade@gmail.com>
2024-12-11 19:41:52 -0700

    Added basic publish widget method

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,12 @@
 import json
 import boto3
 import botocore
+import logging
+
+from botocore.exceptions import ClientError
+
+queue_name = 'cs5250-requests'
+queue = None
 
 def lambda_handler(event, context):
     # TODO implement
@@ -8,3 +14,14 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+def publish_widget(widget_json_str):
+    try:
+        response = queue.send_message(
+            MessageBody=widget_json_str
+        )
+    except ClientError as error:
+        print(f"An error occurred sending {widget_json_str} to the sqs queue.")
+    else:
+        print(response)
+        return response
